e9f775c2b9e9c30c8c5fb838fb9f722880d55b59
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-10 04:41:22 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,7 @@
 print("Hello to this great and prosperous world and to all who inhabit it, ps this is editted")
 print("editted from ipad")
+
+#GPIO22 Right:Forward
+#GPIO23 Right:Backward
+#GPIO24 Right:Forward
+#GPIO25 Right:Backward
