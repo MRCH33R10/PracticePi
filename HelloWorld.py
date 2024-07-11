ff55063b319a3a1952feb944a018b587c465fc1c
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-11 15:12:53 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,7 +5,10 @@ import time
 print("Hello to this great and prosperous world and to all who inhabit it, ps this is editted")
 print("editted from ipad")
 
+#GPIO22 Right:Forward
+#GPIO23 Right:Backward
 motor1 = Motor(22, 23)
+motor2 = Motor(27, 24)
 motor1.forward()
 time.sleep(1)
 motor1.forward(0.5)
@@ -13,6 +16,12 @@ time.sleep(1)
 motor1.forward(0.1)
 time.sleep(1)
 motor1.backward()
+time.sleep(1)
+motor2.forward()
+time.sleep(1)
+motor2.forward(0.5)
+time.sleep(1)
+motor2.forward(0.1)
 time.sleep(1)
 #GPIO22 Right:Forward
 #GPIO23 Right:Backward
