3c03865cce4ac994e2a5cb1e35972eab7d00cbe5
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-11 11:30:14 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,20 +1,19 @@
 from gpiozero import LED
+from gpiozero import Motor
 import time
 
 
 print("Hello to this great and prosperous world and to all who inhabit it, ps this is editted")
 print("editted from ipad")
 
-led1 = LED(22)
-led2 = LED(23)
-
-led1.on()
+motor1 = Motor(22, 23)
+motor1.forward()
 time.sleep(1)
-led1.off()
+motor1.backward()
 time.sleep(1)
-led2.on()
+motor1.forward(0.5)
 time.sleep(1)
-led2.off()
+motor1.backward(0.5)
 time.sleep(1)
 #GPIO22 Right:Forward
 #GPIO23 Right:Backward
