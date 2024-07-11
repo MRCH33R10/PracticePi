636f9e0e89ba4910fc800f7251e21aa6718556d6
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-11 11:00:58 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,21 @@
+from gpiozero import LED
+import time
+
+
 print("Hello to this great and prosperous world and to all who inhabit it, ps this is editted")
 print("editted from ipad")
 
+led1 = LED(22)
+led2 = LED(23)
+while(1):
+  led1.on()
+  time.sleep(1)
+  led1.off()
+  time.sleep(1)
+  led2.on()
+  time.sleep(1)
+  led2.off()
+  time.sleep(1)
 #GPIO22 Right:Forward
 #GPIO23 Right:Backward
 #GPIO24 Right:Forward
