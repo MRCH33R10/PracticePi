4346bd0728152a562e36245be66b2fd16e45f92d
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-12 04:17:01 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -18,30 +18,35 @@ ButtonPin = 25
 GPIO.setmode(GPIO.BCM)
 
 def ButtonPin_callback(channel):
-  ext = False
+    global ext
+    ext = False
   
 GPIO.setup(ButtonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.add_event_detect(ButtonPin, GPIO.BOTH, callback = ButtonPin_callback, bouncetime = 50)
 
-while ext == True:
-  motorR.forward()
-  time.sleep(1)
-  motorR.forward(0.5)
-  time.sleep(1)
-  motorR.forward(0.1)
-  time.sleep(1)
-  motorR.backward()
-  time.sleep(1)
-  
-  motorL.forward()
-  time.sleep(1)
-  motorL.forward(0.5)
-  time.sleep(1)
-  motorL.forward(0.1)
-  time.sleep(1)
-  motorL.backward()
-  time.sleep(1)
-
+try:
+    while ext:
+        motorR.forward()
+        time.sleep(1)
+        motorR.forward(0.5)
+        time.sleep(1)
+        motorR.forward(0.1)
+        time.sleep(1)
+        motorR.backward()
+        time.sleep(1)
+        motorR.stop()
+        
+        motorL.forward()
+        time.sleep(1)
+        motorL.forward(0.5)
+        time.sleep(1)
+        motorL.forward(0.1)
+        time.sleep(1)
+        motorL.backward()
+        time.sleep(1)
+        motorL.stop()
+finally:
+    GPIO.cleanup()
 
 
 #GPIO22 Right:Forward
