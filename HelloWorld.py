5d458c3f98f40ebbe76e6ad2c5a5a890afd3af76
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-21 22:34:19 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -16,45 +16,45 @@ motorL = Motor(27, 24)
 
 stgenum = iter(("blink", "close"))
 i = next(stgenum)
+print(i)
+# def pressed():
+#     i = next(stgenum)
 
-def pressed():
-    i = next(stgenum)
-
-def waitncheck():
-    time.sleep(1)
-    if (i == "close"):
-        raise StopIteration
-        exit()
+# def waitncheck():
+#     time.sleep(1)
+#     if (i == "close"):
+#         raise StopIteration
+#         exit()
     
-def blink_led():
-    while i == "blink":
-        btn.when_pressed = pressed
-        motorR.forward()
-        waitncheck()
-        motorR.forward(0.5)
-        waitncheck()
-        motorR.forward(0.1)
-        waitncheck()
-        motorR.backward()
-        waitncheck()
-        motorR.stop()
+# def blink_led():
+#     while i == "blink":
+#         btn.when_pressed = pressed
+#         motorR.forward()
+#         waitncheck()
+#         motorR.forward(0.5)
+#         waitncheck()
+#         motorR.forward(0.1)
+#         waitncheck()
+#         motorR.backward()
+#         waitncheck()
+#         motorR.stop()
         
-        motorL.forward()
-        waitncheck()
-        motorL.forward(0.5)
-        waitncheck()
-        motorL.forward(0.1)
-        waitncheck()
-        motorL.backward()
-        waitncheck()
-        motorL.stop()
+#         motorL.forward()
+#         waitncheck()
+#         motorL.forward(0.5)
+#         waitncheck()
+#         motorL.forward(0.1)
+#         waitncheck()
+#         motorL.backward()
+#         waitncheck()
+#         motorL.stop()
 
             
-btn = Button(25)
-btn.when_pressed = pressed
+# btn = Button(25)
+# btn.when_pressed = pressed
 
-blink_thread = Thread(target=blink_led)
-blink_thread.start()
+# blink_thread = Thread(target=blink_led)
+# blink_thread.start()
 
 
 
