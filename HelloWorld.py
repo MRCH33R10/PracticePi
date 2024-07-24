38c4621bf8660d4f79d652f223e330519fd4eeb5
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-24 13:52:03 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -30,7 +30,7 @@ def waitncheck():
         
 
 def blink_led():
-    while True:
+    while stgenum:
         motorR.forward()
         waitncheck()
         motorR.forward(0.5)  # Adjust this line if Motor does not support this directly
