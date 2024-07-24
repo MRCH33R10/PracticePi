cdec108353f592e00a9593eb8cf600c737a52fdf
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-24 19:51:58 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -29,9 +29,20 @@ def waitncheck():
         
 def mtr(spdl,spdr):
     if isinstance(spdr, float):
-        motorR.forward(spdr) if spdr > 0 else motorR.forward(abs(spdr)) if spdr < 0 else motorR.stop()
+        if spdr > 0:
+            motorR.forward(spdr)  
+        elif spdr < 0:
+            motorR.forward(abs(spdr))  
+        else: 
+            motorR.stop()
+            
     if isinstance(spdl, float):
-        motorL.forward(spdl) if spdl > 0 else motorL.forward(abs(spdl)) if spdl < 0 else motorL.stop()
+        if spdl > 0:
+            motorL.forward(spdl)  
+        elif spdl < 0:
+            motorL.forward(abs(spdl))  
+        else: 
+            motorL.stop()
             
 def blink_led():
     n = None
