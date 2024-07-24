156ffc0dac2c42b4e25072f604c7fc4dcecfadc1
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-24 01:25:23 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,58 +1,68 @@
-from gpiozero import Motor
+# from gpiozero import Motor
+# from gpiozero import Button
+# from threading import Thread
+# from signal import pause
+# import sys
+# import time
+
+# print("Hello")
+
+# # GPIO22 Right:Forward
+# # GPIO23 Right:Backward
+# motorR = Motor(22, 23)
+# # GPIO27 Left:Forward
+# # GPIO24 Left:Backward
+# motorL = Motor(27, 24)
+
+# stgenum = True
+
+# def pressed():
+#     global stgenum
+#     stgenum = False
+
+# def waitncheck():
+#     time.sleep(1)
+#     if not stgenum:
+#         print("bye")
+#         quit()
+
+# def blink_led():
+#     while stgenum:
+#         motorR.forward()
+#         waitncheck()
+#         motorR.forward(0.5)  # Adjust this line if Motor does not support this directly
+#         waitncheck()
+#         motorR.forward(0.1)  # Adjust this line if Motor does not support this directly
+#         waitncheck()
+#         motorR.backward()
+#         waitncheck()
+#         motorR.stop()
+
+#         motorL.forward()
+#         waitncheck()
+#         motorL.forward(0.5)  # Adjust this line if Motor does not support this directly
+#         waitncheck()
+#         motorL.forward(0.1)  # Adjust this line if Motor does not support this directly
+#         waitncheck()
+#         motorL.backward()
+#         waitncheck()
+#         motorL.stop()
+
+# btn = Button(25)
+# btn.when_pressed = pressed
+
+# blink_thread = Thread(target=blink_led)
+# blink_thread.start()
+
+# pause()
+
 from gpiozero import Button
-from threading import Thread
-from signal import pause
-import sys
-import time
-
-print("Hello")
-
-# GPIO22 Right:Forward
-# GPIO23 Right:Backward
-motorR = Motor(22, 23)
-# GPIO27 Left:Forward
-# GPIO24 Left:Backward
-motorL = Motor(27, 24)
-
-stgenum = True
-
-def pressed():
-    global stgenum
-    stgenum = False
-
-def waitncheck():
-    time.sleep(1)
-    if not stgenum:
-        print("bye")
-        quit()
-
-def blink_led():
-    while stgenum:
-        motorR.forward()
-        waitncheck()
-        motorR.forward(0.5)  # Adjust this line if Motor does not support this directly
-        waitncheck()
-        motorR.forward(0.1)  # Adjust this line if Motor does not support this directly
-        waitncheck()
-        motorR.backward()
-        waitncheck()
-        motorR.stop()
-
-        motorL.forward()
-        waitncheck()
-        motorL.forward(0.5)  # Adjust this line if Motor does not support this directly
-        waitncheck()
-        motorL.forward(0.1)  # Adjust this line if Motor does not support this directly
-        waitncheck()
-        motorL.backward()
-        waitncheck()
-        motorL.stop()
 
 btn = Button(25)
-btn.when_pressed = pressed
 
-blink_thread = Thread(target=blink_led)
-blink_thread.start()
-
-pause()
-
+while True:
+    try:
+        if btn.is_pressed:
+            print("Pressed")
+    except KeyboardInterrupt:
+        print("Ending program")
