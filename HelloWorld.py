c756939e807a45bd8089947737b177877620233b
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-24 15:22:34 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -22,10 +22,9 @@ def pressed():
 
 def waitncheck():
     global stgenum
-    for x in range(9):
-        time.sleep(0.1)
-        if not stgenum:
-            exit()
+    time.sleep(1)
+    if not stgenum:
+        break
 
         
 
