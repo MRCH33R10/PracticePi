a2c3920c789a493cc2d7224859e00842c46b7116
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-25 20:36:33 -0400

    Update RotaryEncoder.py

--- RotaryEncoder.py
+++ RotaryEncoder.py
@@ -1,6 +1,23 @@
-from gpiozero import RotaryEncoder
+from gpiozero import RotaryEncoder, PWMLED
 from gpiozero.tools import scaled_half
 
 rotor = RotaryEncoder(5, 6)
 led = PWMLED(13)
 led.source = scaled_half(rotor.values)
+
+# from gpiozero import RotaryEncoder, PWMLED
+# from gpiozero.tools import scaled_half
+# from signal import pause
+
+# # Set up the rotary encoder on GPIO pins 21 and 20
+# rotor = RotaryEncoder(5, 6)
+
+# # Set up the PWM LED on GPIO pin 5
+# led = PWMLED(13)
+
+# # Link the output of the rotary encoder to the LED's brightness,
+# # scaling the value to half of the encoder's range.
+# led.source = scaled_half(rotor.values)
+
+# # Keep the program running to continue responding to encoder changes
+# pause()
