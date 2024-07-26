2a379651335e21670d5fd95858baa1b4a2787bc4
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-25 20:32:47 -0400

    Update RotaryEncoder.py

--- RotaryEncoder.py
+++ RotaryEncoder.py
@@ -1,6 +1,6 @@
 from gpiozero import RotaryEncoder
 from gpiozero.tools import scaled_half
 
-rotor = RotaryEncoder(21, 20)
-led = PWMLED(5)
+rotor = RotaryEncoder(5, 6)
+led = PWMLED(13)
 led.source = scaled_half(rotor.values)
