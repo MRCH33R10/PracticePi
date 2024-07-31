867794c3d501318335b736dcc0f2dec4d8e615c2
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-31 15:11:31 -0400

    Update SourceAPI.py

--- SourceAPI.py
+++ SourceAPI.py
@@ -9,7 +9,7 @@
 # if __name__ == "__main__":
 #     app.run(debug=True)
 import requests
-r = requests.get('http://google.com')
+r = requests.get('http://127.0.0.1:5000')
 print(r.status_code)
 print(r.headers)
 print(r.text[0:1000])
