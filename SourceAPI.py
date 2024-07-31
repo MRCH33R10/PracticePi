f560385bae49fe0ec37f58b786e8247ebddb3708
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-31 19:01:07 -0400

    Update SourceAPI.py

--- SourceAPI.py
+++ SourceAPI.py
@@ -1,28 +1,3 @@
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def home ():
-#     return "Hello, World!"
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-# import requests
-
-# # Replace '192.168.1.10' with the actual IP address of your Raspberry Pi Zero 2 W
-# raspberry_pi_ip = '192.168.0.125' 
-# r = requests.get(f'http://127.0.0.1:5000')
-# print(r.status_code)
-# print(r.text)
-# import requests
-# import json
-
-# url = 'http://192.168.0.162:5000/data'
-# data = {'key': 'value'}
-
-# response = requests.post(url, json=data)
-# print(f"Response from server: {response.json()}")
 import requests
 import json
 
