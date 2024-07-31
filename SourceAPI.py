ed1a2eb5a86b36eb6f3a2f8838b7ade880abdccf
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-07-31 17:10:40 -0400

    Update SourceAPI.py

--- SourceAPI.py
+++ SourceAPI.py
@@ -8,7 +8,10 @@
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
-raspberry_pi_ip = '192.168.1.10' 
+import requests
+
+# Replace '192.168.1.10' with the actual IP address of your Raspberry Pi Zero 2 W
+raspberry_pi_ip = '192.168.0.125' 
 r = requests.get(f'http://{raspberry_pi_ip}:5000/')
 print(r.status_code)
 print(r.text)
