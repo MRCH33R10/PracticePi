c5f0d8bb2af0dcf40513c6b239cae776e0b43220
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-08-07 12:01:58 -0400

    Create CSVtoMP4.py

---
+++ NetworkInt/CSVtoMP4.py
@@ -0,0 +1,18 @@
+import urllib.request
+import csv
+
+# https://archive.org/download/northplayground/
+
+indx = 0
+filename = input("file path: /Users/nicholasthompson/Desktop/Extracurricular/Southpark1-20 - Sheet1.csv")
+lower = int(input("What is the lower bounds of the season?: "))
+upper = int(input("What is the upper bounds of the season?: "))
+
+with open(filename, 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        if row[0] == ".":
+            indx += 1
+            if (indx>=lower)&(indx<=upper): input("press any key to continue...")
+        elif (indx>=lower)&(indx<=upper):
+            urllib.request.urlretrieve(row[1], row[0])
